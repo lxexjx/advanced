edda7d70268d187ce6871f491764e9bd6f576a6b
lxexjx <eun.leee1377@gmail.com>
2022-03-27 17:52:13 +0900

    Initial commit

---
+++ src/main/java/hello/advanced/vo/OrderControllerV0.java
@@ -0,0 +1,18 @@
+package hello.advanced.vo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@controller+@ResponseBOdy
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
